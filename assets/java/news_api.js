a72c6c86c703458c0d119e776684a23307f5398b
Nicholas Buck <njibuck@gmail.com>
2019-09-28 14:04:13 -0400

    End of class Saturday changes - including renaming newlayout to index

---
+++ assets/java/news_api.js
@@ -0,0 +1,19 @@
+function callNews(destCity) {
+    var apiKey = "0785145288d041dfa635be90e3d35bcb";
+    // Constructing a queryURL
+    var queryURL = "https://newsapi.org/v2/everything?q=" + encodeURIComponent(destCity) + "&sortBy=publishedAt&apiKey=" + apiKey + "&lang='en'";
+    console.log("queryURL: " + queryURL);
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    })
+        // After data comes back from the request
+        .then(function (response) {
+            console.log(response);
+            })
+            // var restaurantP = $("<p>").text("City: " + response.city.name);
+            // weatherP.append("<br/>" + "Conditions: " + response.list[0].weather[0].main);
+            // $(".box-local").prepend(restaurantP);
+            // $("#weatherOutput").prepend(weatherP);
+}
