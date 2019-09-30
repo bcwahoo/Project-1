336e5ac1275f15ea8e7ab8ea1d2ef0a3b1f2bd3c
Nicholas Buck <njibuck@gmail.com>
2019-09-30 00:13:38 -0400

    Code tidy-up + news API working

--- assets/java/news_api.js
+++ assets/java/news_api.js
@@ -1,8 +1,10 @@
 function callNews(destCity) {
     var apiKey = "0785145288d041dfa635be90e3d35bcb";
+
     // Constructing a queryURL
-    var queryURL = "https://newsapi.org/v2/everything?q=" + encodeURIComponent(destCity) + "&sortBy=publishedAt&apiKey=" + apiKey + "&lang='en'";
+    var queryURL = "https://newsapi.org/v2/everything?sources=google-news&q=" + encodeURIComponent(destCity) + "&sortBy=publishedAt&apiKey=" + apiKey;
     console.log("queryURL: " + queryURL);
+    
     // Performing an AJAX request with the queryURL
     $.ajax({
         url: queryURL,
@@ -11,9 +13,20 @@ function callNews(destCity) {
         // After data comes back from the request
         .then(function (response) {
             console.log(response);
-            })
-            // var restaurantP = $("<p>").text("City: " + response.city.name);
-            // weatherP.append("<br/>" + "Conditions: " + response.list[0].weather[0].main);
-            // $(".box-local").prepend(restaurantP);
-            // $("#weatherOutput").prepend(weatherP);
+
+            var newsP = $("<p>");
+
+            $(newsP).append("Headlines for " + destCity + ":");
+
+            for (var i = 0; i < response.articles.length; i++) {
+
+
+                console.log(response.articles[i].title);
+
+                $(newsP).append("<br/>" + "<br/>" + response.articles[i].title);
+            };
+
+            $("#box-news").prepend(newsP);
+
+        })
 }
