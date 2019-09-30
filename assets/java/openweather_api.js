42d55b6e08d9ea6fcf0f0aba999ba796062222c8
Nicholas Buck <njibuck@gmail.com>
2019-09-30 10:42:46 -0400

    Weather icons and front end code tidy up

--- assets/java/openweather_api.js
+++ assets/java/openweather_api.js
@@ -18,62 +18,19 @@ function callOpenWeather(destCity) {
     })
         // After data comes back from the request
         .then(function (response) {
+            console.log(response);
 
-            var weatherP = $("<p>");
+            $("#box-weather").html("");
+
+            var weatherP = $("<div>");
             weatherP.append("<br/>");
-            weatherP.append("<br/>" + (response.list[0].dt_txt).substr(5, 5) + ":  " + (parseInt((response.list[0].main.temp_max - 273.15) * 1.8) + 32) + "F     " + response.list[0].weather[0].main);
-            weatherP.append("<br/>" + (response.list[8].dt_txt).substr(5, 5) + ":  " + (parseInt((response.list[8].main.temp_max - 273.15) * 1.8) + 32) + "F     "+ response.list[8].weather[0].main);
-            weatherP.append("<br/>" + (response.list[16].dt_txt).substr(5, 5) + ":  " + (parseInt((response.list[16].main.temp_max - 273.15) * 1.8) + 32) + "F     "+ response.list[16].weather[0].main);
-            weatherP.append("<br/>" + (response.list[24].dt_txt).substr(5, 5) + ":  " + (parseInt((response.list[24].main.temp_max - 273.15) * 1.8) + 32) + "F     "+ response.list[24].weather[0].main);
-            weatherP.append("<br/>" + (response.list[32].dt_txt).substr(5, 5) + ":  " + (parseInt((response.list[32].main.temp_max - 273.15) * 1.8) + 32) + "F     "+ response.list[32].weather[0].main);
-
-            weatherP.append("<canvas id='myChart' width='200' height='200'></canvas>");
-
+            weatherP.append("<br/>" + (response.list[0].dt_txt).substr(5, 2) + "/" + (response.list[0].dt_txt).substr(8, 2) + ":  " + (parseInt((response.list[0].main.temp_max - 273.15) * 1.8) + 32) + "F     " + response.list[0].weather[0].main + "<img src='http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png' width='50' height='50'>");
+            weatherP.append("<br/>" + (response.list[0].dt_txt).substr(5, 2) + "/" + (response.list[8].dt_txt).substr(8, 2) + ":  " + (parseInt((response.list[8].main.temp_max - 273.15) * 1.8) + 32) + "F     " + response.list[8].weather[0].main + "<img src='http://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + "@2x.png' width='50' height='50'>");
+            weatherP.append("<br/>" + (response.list[0].dt_txt).substr(5, 2) + "/" + (response.list[16].dt_txt).substr(8, 2) + ":  " + (parseInt((response.list[16].main.temp_max - 273.15) * 1.8) + 32) + "F     " + response.list[16].weather[0].main + "<img src='http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png' width='50' height='50'>");
+            weatherP.append("<br/>" + (response.list[0].dt_txt).substr(5, 2) + "/" + (response.list[24].dt_txt).substr(8, 2) + ":  " + (parseInt((response.list[24].main.temp_max - 273.15) * 1.8) + 32) + "F     " + response.list[24].weather[0].main + "<img src='http://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + "@2x.png' width='50' height='50'>");
+            weatherP.append("<br/>" + (response.list[0].dt_txt).substr(5, 2) + "/" + (response.list[32].dt_txt).substr(8, 2) + ":  " + (parseInt((response.list[32].main.temp_max - 273.15) * 1.8) + 32) + "F     " + response.list[32].weather[0].main + "<img src='http://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + "@2x.png' width='50' height='50'>");
 
             $("#box-weather").prepend(weatherP);
-
-            // Graphing output
-            var ctx = document.getElementById('myChart').getContext('2d');
-
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: [
-                        (response.list[0].dt_txt).substr(5, 5),
-                        (response.list[8].dt_txt).substr(5, 5),
-                        (response.list[16].dt_txt).substr(5, 5),
-                        (response.list[24].dt_txt).substr(5, 5),
-                        (response.list[32].dt_txt).substr(5, 5)
-                    ],
-                    datasets: [{
-                        label: 'Temperature in ' + response.city.name,
-                        data: [
-                            parseInt((response.list[0].main.temp_max - 273.15) * 1.8) + 32,
-                            parseInt((response.list[8].main.temp_max - 273.15) * 1.8) + 32,
-                            parseInt((response.list[16].main.temp_max - 273.15) * 1.8) + 32,
-                            parseInt((response.list[24].main.temp_max - 273.15) * 1.8) + 32,
-                            parseInt((response.list[32].main.temp_max - 273.15) * 1.8) + 32
-                        ],
-                        backgroundColor:
-                            'rgba(54, 162, 235, 0.4)',
-                        borderColor:
-                            'rgba(54, 162, 235, 1)',
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
-
-            $("#box-weather").append(myChart);
-
         });
 }
 
